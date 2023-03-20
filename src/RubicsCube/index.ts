9f188a11b952499c7ab7107a56e7a6b507ca521b
pedeEli <74102959+pedeEli@users.noreply.github.com>
2023-03-20 21:31:07 +0100

    added default texture, uvs and hoveringColors

--- src/RubicsCube/index.ts
+++ src/RubicsCube/index.ts
@@ -1,3 +1,24 @@
 import {RubicsCube} from './RubicsCube'
 
 export default RubicsCube
+
+
+export const defaultTexture = new ImageData(new Uint8ClampedArray([
+  0,   0, 255, 255,
+  0, 255,   0, 255,
+255, 255,   0, 255,
+255, 255, 255, 255,
+255,   0,   0, 255,
+255, 127,   0, 255
+]), 1, 6)
+
+export const defaultUVs = Array(6).fill(null).map((_, index) => {
+return [
+  0, (index + 0) / 6,
+  1, (index + 0) / 6,
+  1, (index + 1) / 6,
+  0, (index + 1) / 6
+]
+})
+
+export const defaultHovovingColors = Array(6).fill(Array(3).fill(.7))
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,13 @@
 import './style.css'
-import RubicsCube from './RubicsCube'
+import RubicsCube, {defaultHovovingColors, defaultTexture, defaultUVs} from './RubicsCube'
 
-const image = new ImageData(new Uint8ClampedArray([
-    0,   0, 155, 255,     0,   0, 255, 255,
-    0,   0, 255, 255,     0,   0, 255, 255,
-    0, 155,   0, 255,     0, 255,   0, 255,
-    0, 255,   0, 255,     0, 255,   0, 255,
-  155, 155,   0, 255,   255, 255,   0, 255,
-  255, 255,   0, 255,   255, 255,   0, 255,
-  155, 155, 155, 255,   255, 255, 255, 255,
-  255, 255, 255, 255,   255, 255, 255, 255,
-  155,   0,   0, 255,   255,   0,   0, 255,
-  255,   0,   0, 255,   255,   0,   0, 255,
-  155,  77,   0, 255,   255, 127,   0, 255,
-  255, 127,   0, 255,   255, 127,   0, 255,
-]), 2, 12)
 
-const uvs = Array(6).fill(null).map((_, index) => {
-  return [
-    0, (index + 0) / 6,
-    1, (index + 0) / 6,
-    1, (index + 1) / 6,
-    0, (index + 1) / 6
-  ]
-})
-
-const hovovingColors = Array(6).fill(Array(3).fill(.7))
 
 const rubicsCube = new RubicsCube(
   'data-rubics-cube',
-  image,
-  uvs,
-  hovovingColors
+  defaultTexture,
+  defaultUVs,
+  defaultHovovingColors
 )
 
 rubicsCube.start()
