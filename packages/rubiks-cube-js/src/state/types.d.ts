120f5170485e393393868a143e993d978be482dc
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-04-12 06:35:33 +0200

    changed fucking setting withing git so my renaming is actually recognised

---
+++ packages/rubiks-cube-js/src/state/types.d.ts
@@ -0,0 +1,19 @@
+export type TurnBase = UD | RL | FB
+export type Turn = TurnBase | `${TurnBase}2` | `${TurnBase}'`
+
+export type UD = 'U' | 'D'
+export type RL = 'R' | 'L'
+export type FB = 'F' | 'B'
+
+export type Corner = `${UD}${RL}${FB}`
+export type CornerOrientation = 0 | 1 | 2
+
+export type Edge = `${UD}${FB | RL}` | `${FB}${RL}`
+export type EdgeOrientation = 0 | 1
+
+export type Center = UD | RL | FB
+export type CenterOrientation = 0 | 1 | 2 | 3
+
+export type Events = {
+	change: import('.').State
+}
