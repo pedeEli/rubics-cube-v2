c5cd51c654db8ee658319b4f641205d1359a5591
Elias Gerster <74102959+pedeEli@users.noreply.github.com>
2024-04-12 06:17:17 +0200

    changed www to not use number.png

--- packages/www/main.js
+++ packages/www/main.js
@@ -3,58 +3,33 @@ import './style.css'
 import {RubiksCube, defaultHovorvingColors, defaultTexture, defaultUVs} from 'rubiks-cube-js'
 
 
-const image = new Image()
-image.src = 'number.png'
+const rubiksCube = new RubiksCube(
+  'data-rubiks-cube',
+  defaultTexture,
+  defaultUVs,
+  defaultHovorvingColors,
+  false
+)
 
-const uvs = Array(6).fill(null).map((_, side) => {
-  const bottom = 0.5 + Math.floor(side / 3) * 0.5
-  const left = (side % 3) / 3
-  return Array(9).fill(null).map((_, sideIndex) => {
-    const b = bottom - (sideIndex % 3) / 6
-    const t = b - 1 / 6
-    const l = left + Math.floor(sideIndex / 3) / 9
-    const r = l + 1 / 9
-    return [
-      l, b,
-      r, b,
-      r, t,
-      l, t
-    ]
-  })
-})
+const url = new URL(location.toString())
+const state = url.searchParams.get('state')
+if (state != null && !rubiksCube.setState(state)) {
+  rubiksCube.reset()
+  url.searchParams.delete('state')
+  history.replaceState(null, '', url)
+}
 
-image.addEventListener('load', () => {
-  const rubiksCube = new RubiksCube(
-    'data-rubiks-cube',
-    image,
-    uvs,
-    defaultHovorvingColors,
-    true
-  )
-  
+rubiksCube.on('change', event => {
   const url = new URL(location.toString())
-  const state = url.searchParams.get('state')
-  if (state != null && !rubiksCube.setState(state)) {
-    rubiksCube.reset()
-    url.searchParams.delete('state')
-    history.replaceState(null, '', url)
-  }
-
-  rubiksCube.on('change', event => {
-    const url = new URL(location.toString())
-    url.searchParams.set('state', event.state.toString())
-    history.replaceState(null, '', url)
-  })
-
-  rubiksCube.start()
+  url.searchParams.set('state', event.state.toString())
+  history.replaceState(null, '', url)
 })
+
+rubiksCube.start()
 
 document.querySelector('[data-reset-state]')?.addEventListener('click', () => {
+  rubiksCube.reset()
   const url = new URL(location.toString())
   url.searchParams.delete('state')
-  location.replace(url)
-})
-
-document.querySelector('[data-reload]')?.addEventListener('click', () => {
-  location.reload()
+  history.replaceState(null, '', url)
 })
